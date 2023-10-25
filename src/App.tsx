10ac76ec70ba7f6592105a11a6fc2be33e9fca6a
brygmos <daniel-best@yandex.ru>
2023-10-25 15:24:39 +0300

    add theme vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css'
 import Sidebar from "./components/Sidebar/Sidebar.tsx";
 import Main from "./components/Main/Main.tsx";
+import {useEffect} from "react";
 
 function App() {
+
+    useEffect(() => {
+        document.body.className = 'app_dark_theme';
+        // localStorage.setItem(LOCAL_STORAGE_THEME_KEY, theme);
+    }, []);
 
     return (
         <>
