34d1fd0cfcb009d4c3199c0c49848ff023a8fa71
brygmos <daniel-best@yandex.ru>
2023-10-26 16:19:56 +0300

    fix: style

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -28,7 +28,7 @@ const Sidebar = () => {
             <h2>Мои заметки</h2>
             <button onClick={addHandler} className={cls.addButton}>
                 <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M6 12H18M12 6V18" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                    <path d="M6 12H18M12 6V18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
             </button>
             {notes && notes.map((note) => <SidebarCard key={note.title} note={note}/>)}
