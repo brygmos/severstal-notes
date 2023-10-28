359c9d79ef1370996c594ae3249c17d725e0bba1
brygmos <daniel-best@yandex.ru>
2023-10-28 19:31:28 +0300

    fix: a11y; remove: notChange error

--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -9,6 +9,7 @@ const TextEditor = () => {
     const dispatch = useAppDispatch()
     const [text, setText] = useState('');
     const [title, setTitle] = useState('');
+    const [titleEmpty, setTitleEmpty] = useState(false);
 
     const note: Note = useAppSelector(selectActiveNote)
     const notUniqueTitle: boolean = useAppSelector(selectActiveNoteNotUnique)
@@ -27,22 +28,23 @@ const TextEditor = () => {
     };
 
     const saveHandler = () => {
+        if(!title) return setTitleEmpty(true); else setTitleEmpty(false)
         isEditing
             ? dispatch(editNote({text: text, title: title}))
             : dispatch(addNote({text: text, title: title}))
     }
 
     const closeHandler = () => {
+        setTitleEmpty(false)
         document.querySelector('main')?.classList.add('hide')
     }
-
-    const saveDisabled = !text || !title || (text == note.text && title == note.title)
 
     return (
         <div className={cls.editor}>
             <div className={cls.buttonsBlock}>
-                <button onClick={saveHandler} disabled={saveDisabled}>Сохранить</button>
+                <button onClick={saveHandler}>Сохранить</button>
                 <button onClick={closeHandler}>Закрыть</button>
+                {titleEmpty && <span>Введите заголовок</span>}
                 {notUniqueTitle && <span>Такое название уже существует</span>}
                 {!notUniqueTitle && <span className={cls.dummy}></span>}
             </div>
